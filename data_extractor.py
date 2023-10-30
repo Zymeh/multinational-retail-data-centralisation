a4cb5dd77df818b0a413d67b96cd86eb2adbdaf3
Kingsley Kingdon <kinqsleyy@gmail.com>
2023-10-30 14:26:32 +0000

    created a function that lists tables

--- data_extractor.py
+++ data_extractor.py
@@ -1,1 +1,20 @@
 class DataExtractor:
+
+    def __init__(self, file_path):
+        import database_utils
+        self.file = file_path
+        self.db_con = database_utils.DatabaseConnector(self.file)
+
+    def list_db_tables(self):
+        from sqlalchemy import MetaData, inspect
+
+        engine = self.db_con.init_db_engine()
+        inspector = inspect(engine)
+
+        return inspector.get_table_names()
+
+        
+
+db = DataExtractor('db_creds.yaml')
+
+print(db.list_db_tables())
