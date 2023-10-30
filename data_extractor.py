216afa8e2bf70863b1dedcdf6812db0ad9c58160
Kingsley Kingdon <kinqsleyy@gmail.com>
2023-10-30 15:17:15 +0000

    created a function that reads the RDS and creates a dictionary of pandas tables

--- data_extractor.py
+++ data_extractor.py
@@ -4,17 +4,21 @@ class DataExtractor:
         import database_utils
         self.file = file_path
         self.db_con = database_utils.DatabaseConnector(self.file)
+        self.engine = self.db_con.init_db_engine()
 
-    def list_db_tables(self):
+    def list_db_tables(self): #lists tables
         from sqlalchemy import MetaData, inspect
 
-        engine = self.db_con.init_db_engine()
-        inspector = inspect(engine)
+        inspector = inspect(self.engine)
 
         return inspector.get_table_names()
 
-        
+    def read_rds_table(self):
+        import pandas as pd
+        list_of_table_names = self.list_db_tables()
+        dataframes = {}
 
-db = DataExtractor('db_creds.yaml')
-
-print(db.list_db_tables())
+        for table in list_of_table_names:
+            df = pd.read_sql_table(table, self.engine)
+            dataframes.update({table: df})
+        return dataframes
