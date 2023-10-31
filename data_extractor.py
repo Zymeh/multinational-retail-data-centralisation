edb73a4ff81e0397d6a0052e226710e40a0a3e76
Kingsley Kingdon <kinqsleyy@gmail.com>
2023-10-31 16:01:31 +0000

    created a method which extracts data and turns it into a pandas dataframe

--- data_extractor.py
+++ data_extractor.py
@@ -1,10 +1,14 @@
+from database_utils import DatabaseConnector
+import tabula
+import validators
+import pandas as pd
 class DataExtractor:
 
-    def __init__(self, file_path):
-        import database_utils
-        self.file = file_path
-        self.db_con = database_utils.DatabaseConnector(self.file)
-        self.engine = self.db_con.init_db_engine()
+    def __init__(self):
+        
+        self.file = 'db_creds.yaml'
+        self.rds_db_con = DatabaseConnector(self.file)
+        self.engine = self.rds_db_con.init_db_engine()
 
     def list_db_tables(self): #lists tables
         from sqlalchemy import MetaData, inspect
@@ -13,8 +17,25 @@ class DataExtractor:
 
         return inspector.get_table_names()
 
-    def read_rds_table(self, table):
-        import pandas as pd
+    def read_rds_table(self, table): #sql table to pandas dataframe
+        
 
         df = pd.read_sql_table(table, self.engine)
         return df
+    
+    def retrieve_pdf_data(self, link):
+        class ValidationError(Exception):
+            pass
+
+        try:
+            if not validators.url(link):
+                raise ValidationError
+        except ValidationError:
+            print(f'The URL ({link}) you have provided is invalid, please try again.')
+   
+        pdf_data = tabula.read_pdf(link, pages='all',multiple_tables=True)
+
+        df = pd.concat(pdf_data, ignore_index=True)
+
+        return df
+    
